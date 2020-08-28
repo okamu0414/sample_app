d81b65a2610823368c22d84f7b5f807eeac9f846
okamu0414 <s.okamuu@icloud.com>
2020-08-28 10:24:08 +0000

    Add login suport

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
       redirect_to user
     else
@@ -13,7 +13,9 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
