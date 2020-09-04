952caec988b1e1476fa3365c80532a05cdf0bf47
okamu0414 <s.okamuu@icloud.com>
2020-09-04 07:14:34 +0000

    Add forget method and Update log_out method

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,13 +24,18 @@ class User < ApplicationRecord
   end
   
   #永続セッションのためにユーザーをデータベースに記憶する
-    def remember
-      self.remember_token = User.new_token
-      update_attribute(:remember_digest, User.digest(remember_token))
-    end
-    
-    #渡されたトークンがダイジェストと一致したらTRUEを返す
-    def authenticated?(remember_token)
-      BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  #渡されたトークンがダイジェストと一致したらTRUEを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  #ユーザのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
